ce9da357247fb73ab3d91ee5d3ad772347bc9c31
Alif108 <46017890+Alif108@users.noreply.github.com>
2019-01-30 01:27:20 +0600

    Add files via upload

--- Project.java
+++ Project.java
@@ -1,27 +1,54 @@
 package project;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Project extends Application {
     
+    Stage PrimaryStage;
+    
     @Override
     public void start(Stage stage) throws Exception {
+        
+        PrimaryStage = new Stage();
+        
+        PrimaryStage = stage;
         
         Parent root = FXMLLoader.load(getClass().getResource("Project.fxml"));
         
         Scene scene = new Scene(root);
         
-        stage.setScene(scene);
+        PrimaryStage.setScene(scene);
         
-        stage.show();
+        PrimaryStage.show();
     }
-
+    /*
+    public void ViewHome() throws IOException
+    {
+    // XML Loading using FXMLLoader
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource("Project.fxml"));
+    Parent root = loader.load();
+    
+    // Loading the controller
+    ProjectController controller = loader.getController();
+    controller.setMain(this);
+    
+    // Set the primary stage
+    PrimaryStage.setTitle("Login");
+    PrimaryStage.setScene(new Scene(root, 400, 250));
+    PrimaryStage.show();
+    }
+    */
     public static void main(String[] args) {
         launch(args);
+        
+        
     }
     
 }
--- SignUpController.java
+++ SignUpController.java
@@ -5,9 +5,18 @@
  */
 package project;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,9 +25,25 @@ import javafx.fxml.Initializable;
  */
 public class SignUpController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    
+    private Button CreateAccountButton;
+    
+    @FXML
+    public void GoToHome(ActionEvent event) throws IOException
+     {
+        
+        Parent home = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+        
+        Scene scene2 = new Scene(home);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(scene2);
+        
+        window.show();
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
